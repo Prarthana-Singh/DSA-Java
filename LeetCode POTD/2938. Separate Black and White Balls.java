e7ea1e5c193f88971b7582ae1e8db17da8f19bfc
Prarthana Singh <143778905+Prarthana-Singh@users.noreply.github.com>
2024-10-15 10:27:00 +0530

    Create 2938. Separate Black and White Balls.java

---
+++ LeetCode POTD/2938. Separate Black and White Balls.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long cnt=0, ans=0;
+        for(int i = s.length()-1;i>=0;i--){
+            if(s.charAt(i)=='1') ans+=cnt;
+            else cnt++;
+        }
+        return ans;
+    }
+}
